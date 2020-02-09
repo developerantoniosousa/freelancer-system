61ea2a8a1da79983b005668b9e3b3c5cd6b791ce
Antonio Sousa <developerantoniosousa@gmail.com>
2020-02-09 11:26:11 -0300

    feat: add create-clients migration

---
+++ src/database/migrations/20200209142309-create-clients.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("clients", {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable("clients");
+  }
+};
