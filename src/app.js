fbc52c659facdb5b92fe09e0f2e87989198d8784
Antonio Sousa <developerantoniosousa@gmail.com>
2020-02-09 10:54:48 -0300

    feat: add express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 import "dotenv/config";
+import express from "express";
+
+class App {
+  constructor() {
+    this.server = express();
+  }
+}
+
+export default new App().server;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,3 @@
 import server from "./app";
+
+server.listen(process.env.PORT);
